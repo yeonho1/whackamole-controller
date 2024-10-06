a2b245b5021f153d8acf4476bce0b19999cca365
Yeonho Lee <yhl080519@gmail.com>
2024-10-06 22:09:46 +0900

    Remove unnecessary comments

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ const loadScreen = (w, name) => {
 }
 
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 1500,
         height: 1100,
@@ -76,18 +75,11 @@ app.on('ready', () => {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     app.quit()
 })
 
 app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
